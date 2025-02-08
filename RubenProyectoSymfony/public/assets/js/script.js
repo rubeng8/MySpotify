b5efa1ab3d56968c39065cd72cf1f9754fd8de2e
ruben <rgj565@educa.madrid.org>
2025-02-08 09:48:47 +0100

    08/02/2025

--- RubenProyectoSymfony/public/assets/js/script.js
+++ RubenProyectoSymfony/public/assets/js/script.js
@@ -2,7 +2,6 @@ function playSong(tituloCancion) {
     const audio = document.getElementById("audioPlayer");
     const cancionRuta = document.getElementById("srcCancion");
     
-    // Ruta donde se encuentra el archivo de la canción (Symfony debe estar sirviendo el archivo con esta ruta)
     cancionRuta.src = `/cancion/${tituloCancion}`;
 
     audio.style.display = "inline-block";
@@ -10,3 +9,43 @@ function playSong(tituloCancion) {
     audio.load();
     audio.play();
 }
+
+
+function buscarCancion() {
+
+    console.log("hola")
+
+    let input = document.getElementById("inputBuscar").value.toLowerCase();
+    let canciones = document.getElementsByClassName("cancion");
+    let encontrado = false;
+
+    for (let i = 0; i < canciones.length; i++) {
+        let titulo = canciones[i].querySelector("h3").textContent.toLowerCase();
+
+        if (titulo.includes(input)) {
+            canciones[i].style.display = "block"; // Mostrar si coincide
+            encontrado = true;
+        } else {
+            canciones[i].style.display = "none"; // Ocultar si no coincide
+        }
+    }
+
+    // Manejar mensaje de "No se encontraron canciones"
+    let sinResultados = document.getElementById("sinResultados");
+
+    if (!encontrado) {
+        if (!sinResultados) {
+            sinResultados = document.createElement("p");
+            sinResultados.id = "sinResultados";
+            sinResultados.textContent = "No se encontraron canciones.";
+            sinResultados.style.color = "white";
+            sinResultados.style.textAlign = "center";
+            document.getElementById("centro").appendChild(sinResultados);
+        }
+    } else {
+        if (sinResultados) {
+            sinResultados.remove();
+        }
+    }
+}
+

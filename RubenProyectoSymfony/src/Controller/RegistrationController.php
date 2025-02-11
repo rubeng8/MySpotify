111996794a99d032270c7b449d080d9273b7449c
ruben <rubeen.gj@hotmail.com>
2025-02-11 18:37:09 +0100

    11/01/2025

---
+++ RubenProyectoSymfony/src/Controller/RegistrationController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistroController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Hash the plain password
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+            $user->setRoles($form->get('roles')->getData());
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_music');
+        }
+        return $this->render('registation/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+}
